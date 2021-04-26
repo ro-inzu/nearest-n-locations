cb5b656baa7a8215b3be5ebe0a03ba03c6944ec2
Rosendo <rosendoinzunza@cloud.com>
2021-04-26 14:14:45 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# nearest-n-locations
+# nearest N locations
+
+Given a list of locations which contains X and Y for distance return the nearest N locations using a min heap based on distance.
+
+## Authors
+
+- [@roinzunza](https://www.github.com/roinzunza)
---
+++ nearest-n-locations.py
@@ -0,0 +1,30 @@
+import heapq, math
+
+# Return the 2 nearest locations
+
+# get the distance for each location
+# return locations based on 2 shortest ones
+
+def nearest_n_locations(allLocations: list,numLocations: int) -> list:
+    distance_each_location = []
+    if len(allLocations) == 0:
+        return distance_each_location
+
+    heapq.heapify(distance_each_location)
+
+    for locations in allLocations:
+        sol = pow(locations[0],2) + pow(locations[1],2)
+        distance = math.sqrt(sol)
+        heapq.heappush(distance_each_location,[distance,locations])
+
+    nearest = [heapq.heappop(distance_each_location)[1] for x in range(0,2) ]
+    return nearest
+
+
+
+
+allLocations = [[1,2],[3,4],[1,-1]]
+numLocations = 2
+nearest = nearest_n_locations(allLocations,numLocations)
+
+print(nearest)
