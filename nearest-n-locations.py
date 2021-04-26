ea897d64b4dfb3710b08636270b7fc5a440911e1
Rosendo <rosendoinzunza@cloud.com>
2021-04-26 14:33:54 -0700

    update.

--- nearest-n-locations.py
+++ nearest-n-locations.py
@@ -6,18 +6,19 @@ import heapq, math
 # return locations based on 2 shortest ones
 
 def nearest_n_locations(allLocations: list,numLocations: int) -> list:
-    distance_each_location = []
+    nearest = []
     if len(allLocations) == 0 and len(allLocations) > numLocations:
-        return distance_each_location
+        return nearest
 
-    heapq.heapify(distance_each_location)
+    heap_list = []
+    heapq.heapify(heap_list)
 
     for locations in allLocations:
         sol = pow(locations[0],2) + pow(locations[1],2)
         distance = math.sqrt(sol)
-        heapq.heappush(distance_each_location,[distance,locations])
+        heapq.heappush(heap_list,[distance,locations])
 
-    nearest = [heapq.heappop(distance_each_location)[1] for x in range(0,numLocations) ]
+    nearest = [heapq.heappop(heap_list)[1] for x in range(0,numLocations) ]
     return nearest
 
 
